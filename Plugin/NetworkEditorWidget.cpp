0a8d1903ce33b08280db5694ff86b4ef182c2049
lhofmann <lhofmann@users.noreply.github.com>
2019-10-13 00:22:22 +0200

    Add style sheet

--- Plugin/NetworkEditorWidget.cpp
+++ Plugin/NetworkEditorWidget.cpp
@@ -4,7 +4,8 @@
 #include "NetworkEditorView.h"
 
 #include <pqApplicationCore.h>
-
+#include <QApplication>
+#include <QFile>
 #include <QMainWindow>
 #include <QProcessEnvironment>
 
@@ -12,9 +13,17 @@
 
 void NetworkEditorWidget::constructor()
 {
+  auto app = dynamic_cast<QApplication*>(QApplication::instance());
+  if (app) {
+    QFile styleSheetFile(":/resources/dark.qss");
+    styleSheetFile.open(QFile::ReadOnly);
+    QString styleSheet = QString::fromUtf8(styleSheetFile.readAll());
+    app->setStyleSheet(styleSheet);
+    styleSheetFile.close();
+  }
+
   networkEditor_ = std::make_unique<NetworkEditor>();
   networkEditorView_ = new NetworkEditorView(networkEditor_.get(), this);
-  // NetworkEditorObserver::addObservation(networkEditor_.get());
 
   bool grab_center_widget = QProcessEnvironment::systemEnvironment().value("NETWORK_EDITOR_DOCK", "0") == "0";
   if (grab_center_widget) {
