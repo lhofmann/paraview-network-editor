28a21e8d33e9633ea7e68cae7e9c8708f5fbaf1f
lhofmann <lhofmann@users.noreply.github.com>
2019-10-12 19:37:38 +0200

    Swap places with central widget

--- Plugin/NetworkEditor.cxx
+++ Plugin/NetworkEditor.cxx
@@ -1,8 +1,30 @@
 #include "NetworkEditor.h"
 
+#include <pqApplicationCore.h>
+
+#include <QMainWindow>
+#include <QProcessEnvironment>
+
+#include <iostream>
+
 void NetworkEditor::constructor()
 {
-  this->setWindowTitle("Network Editor");
   QWidget* t_widget = new QWidget(this);
-  this->setWidget(t_widget);
+
+  bool grab_center_widget = QProcessEnvironment::systemEnvironment().value("NETWORK_EDITOR_DOCK", "0") == "0";
+  if (grab_center_widget) {
+    auto main_window = dynamic_cast<QMainWindow *>(this->parent());
+    if (!main_window) {
+      grab_center_widget = false;
+    } else {
+      this->setWindowTitle("Render View");
+      auto render_view = main_window->takeCentralWidget();
+      main_window->setCentralWidget(t_widget);
+      this->setWidget(render_view);
+    }
+  }
+  if (!grab_center_widget) {
+    this->setWindowTitle("Network Editor");
+    this->setWidget(t_widget);
+  }
 }
