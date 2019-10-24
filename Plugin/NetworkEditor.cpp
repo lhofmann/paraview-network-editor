9c900b39d60ad14b9e1c2fbd770cdfcecd63485e
lhofmann <lhofmann@users.noreply.github.com>
2019-10-24 21:28:11 +0200

    Add right-clicked sources to selection

--- Plugin/NetworkEditor.cpp
+++ Plugin/NetworkEditor.cpp
@@ -306,6 +306,7 @@ void NetworkEditor::contextMenuEvent(QGraphicsSceneContextMenuEvent* e) {
   QMenu menu;
   for (auto& item : items(e->scenePos())) {
     if (auto source = qgraphicsitem_cast<SourceGraphicsItem*>(item)) {
+      source->setSelected(true);
       auto editName = menu.addAction(tr("Edit Name"));
       connect(editName, &QAction::triggered, [this, source]() {
         clearSelection();
